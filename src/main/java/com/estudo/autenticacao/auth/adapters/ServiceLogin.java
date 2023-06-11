fe0b5e58b5e1693eb8ae2988fbdfa930d9c1529a
Maxwell <maxwelldev2021@gmail.com>
2023-06-11 17:00:04 -0300

    chore:habilitado a dependencia do spring security, implementado a classe UserDetails e adicionado o fluxo dentro do service

--- src/main/java/com/estudo/autenticacao/auth/adapters/ServiceLogin.java
+++ src/main/java/com/estudo/autenticacao/auth/adapters/ServiceLogin.java
@@ -1,15 +1,29 @@
 package com.estudo.autenticacao.auth.adapters;
 
+import com.estudo.autenticacao.auth.model.Usuario;
 import com.estudo.autenticacao.auth.resources.dto.FormLogin;
 import com.estudo.autenticacao.auth.resources.dto.ResponseLogin;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
 public class ServiceLogin {
-    public ResponseLogin getToken(FormLogin login) {
 
+    private final AuthenticationManager authenticationManager;
+
+    public ServiceLogin(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    public ResponseLogin getToken(FormLogin login) {
+        var userNamePaswdToken = new UsernamePasswordAuthenticationToken(login.user(), login.password());
+
+        var autenticated = this.authenticationManager.authenticate(userNamePaswdToken);
+
+        var usuario = (Usuario) autenticated.getPrincipal();
 
         return new ResponseLogin("dGVzdGU=", LocalDateTime.now().plusDays(1).toString());
     }
---
+++ src/main/java/com/estudo/autenticacao/auth/model/Usuario.java
@@ -0,0 +1,60 @@
+package com.estudo.autenticacao.auth.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class Usuario implements UserDetails {
+
+    private String nome;
+    private String senha;
+
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return getNome();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
